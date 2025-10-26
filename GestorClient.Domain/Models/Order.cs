96412abf23568850e789a4fcbf3640fb78c7c9d3
jeropq16 <jeronimopq1611@gmail.com>
2025-10-26 17:33:14 -0500

    Creacion de modelos, implementacion de clases, relaciones y validaciones basicas y conexion a la Db

--- GestorClient.Domain/Models/Order.cs
+++ GestorClient.Domain/Models/Order.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GestorClient.Domain.Models;
 
 public class Order
 {
-   
+    public int Id { get; set; }
+
+    [Required]
+    public int CustomerId { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+
+    [Required]
+    public string Status { get; set; }
+
+    public Customer Customer { get; set; }
+
+    public List<OrderDetail> OrderDetails { get; set; }
 }
--- GestorClient.Infrastructure/Data/AppDbContext.cs
+++ GestorClient.Infrastructure/Data/AppDbContext.cs
@@ -7,5 +7,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
     
-    
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
 }
